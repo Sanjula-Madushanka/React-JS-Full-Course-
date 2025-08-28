ac7f3bc03ed01b0cf24a91a2a2ec11e1be58df3e
Sanjula Madushanka <166517153+Sanjula-Madushanka@users.noreply.github.com>
2025-08-28 11:14:23 +0530

    Template literals

--- main.js
+++ main.js
@@ -7,3 +7,10 @@ let a = false;
 let b = true;
 
 console.log(a || getName("sanjula madushanka"));
+
+//Template literals
+
+let name1 = "John";
+let name2 = "Deo";
+
+console.log(name1+" "+name2 , `${name1} ${name2}`);

141f4c1de29c42e9d1ee5295c053b7d55e317653
Sanjula Madushanka <166517153+Sanjula-Madushanka@users.noreply.github.com>
2025-08-28 11:24:04 +0530

    Ternary operator

--- main.js
+++ main.js
@@ -14,3 +14,25 @@ let name1 = "John";
 let name2 = "Deo";
 
 console.log(name1+" "+name2 , `${name1} ${name2}`);
+
+//Ternary operator
+
+const showRecipeOne = false;
+
+function getRecipeOneName(recipeName){
+    return recipeName;
+}
+
+function getRecipeTwoName(recipeName){
+    return recipeName;
+}
+
+if (showRecipeOne){
+    console.log(getRecipeOneName("Pizza"));
+}else{
+    console.log(getRecipeTwoName("coke"));
+}
+
+showRecipeOne
+ ? console.log(getRecipeOneName("Pizza")) 
+ : console.log(getRecipeTwoName("coke"));

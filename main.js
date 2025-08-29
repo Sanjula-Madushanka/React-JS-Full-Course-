bc934a76903b118785f70c4d77ed4500753348e8
Sanjula Madushanka <166517153+Sanjula-Madushanka@users.noreply.github.com>
2025-08-29 10:17:04 +0530

    Object and array destructuring

--- main.js
+++ main.js
@@ -36,3 +36,48 @@ if (showRecipeOne){
 showRecipeOne
  ? console.log(getRecipeOneName("Pizza")) 
  : console.log(getRecipeTwoName("coke"));
+
+
+//Object and array destructuring
+
+const id ="1";
+const productName = "Laptop";
+const rating = 4;
+
+const product ={
+    id :id,
+    productName : productName,
+    rating : rating
+}
+
+console.log(product);
+
+const product2={
+    discription :"product two discription",
+    id,
+    productName,
+    rating  
+}
+
+//const getProductTwoDiscription= product2.discription;
+
+//console.log(getProductTwoDiscription);
+
+const {discription}=product2;
+
+console.log(discription);
+
+
+
+
+const array=[1,2,3];
+
+let getArrayFirstValue = array[0];
+let getArraySecondValue = array[1];
+let getArrayThirdValue = array[2];
+
+console.log(getArrayFirstValue , getArraySecondValue);
+
+const[arrayFirstValue,arraySecondValue,arrayThirdValue]=array;
+
+console.log(arrayFirstValue , arraySecondValue, arrayThirdValue);
